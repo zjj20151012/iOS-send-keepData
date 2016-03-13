8977b629e8e91a33911f0727d9c9deb4a034e60a
zjjbitbucket <443411162@qq.com>
2016-03-13 22:13:47 +0800

    Initial Commit

---
+++ xibTableView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  xibTableView
+//
+//  Created by zjj on 16/3/13.
+//  Copyright © 2016年 zjj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ xibTableView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  xibTableView
+//
+//  Created by zjj on 16/3/13.
+//  Copyright © 2016年 zjj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
